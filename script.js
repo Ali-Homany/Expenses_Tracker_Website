b57e62b773cb8eeee1d31aa9d383fff6c7b20a27
homanydata <houmani.datalu@gmail.com>
2023-11-27 16:10:40 +0200

    version 1.0

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const price = document.getElementById("price")
 const category = document.getElementById("category")
 const submit = document.getElementById("submit")
 const output = document.querySelector(".output")
+const controls = document.getElementsByClassName("controls")[0]
 const search = document.getElementById("search")
 const summary = document.getElementById("summary")
 let mode = "create"
@@ -43,7 +44,6 @@ function submitItem (){
         temp = null
         mode = "create"
         submit.innerHTML = "create"
-        count.style.display = "block"
     }
     data.sort(sortDates)
     // clear values from create form
@@ -59,6 +59,23 @@ function sortDates(a,b){
     return d1 - d2
 }
 function isDateValid(datestr) {
+    let date = datestr.split('/')
+    let day = date[2]
+    let month = date[1]
+    let year = date[0]
+    if(month==2){
+        if(day>29){
+            return false
+        }
+        if(day==29){
+            if(year%4!=0 || (year%100==0 && year%400!=0)){
+                return false
+            }
+        }
+    }
+    if(day==31 && (month==4 || month==6 || month==9 || month==11)){
+        return false
+    }
     return true
     // need to search and check if date is valid
 }
@@ -72,7 +89,7 @@ function createAllRows(data){
             td.innerHTML = value;
             tr.appendChild(td);
         }
-
+        tr.innerHTML += `<td><svg onclick="updateRow(${i})" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg><svg onclick="deleteRow(${i})" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg></td>`
         tbody.appendChild(tr);
     }
     del.innerHTML = "(" + (data.length) + ")";
@@ -98,9 +115,11 @@ function loadData(){
 function displayOutput(){
     if(tbody.childNodes.length > 0){
         output.style.display = "block"
+        controls.style.display = "block"
         summary.style.display = "flex"
     }else{
         output.style.display = "none"
+        controls.style.display = "none"
         summary.style.display = "none"
     }
 }
@@ -136,13 +155,15 @@ function updateRow(id){
     price.value = row.price
     category.value = row.category
     
+    date = row.date.split('/')
+    day.value = date[2]
+    month.value = date[1]
+    year.value = date[0]
+    
     mode = "update"
     submit.innerHTML = "update"
     temp = id
-    window.scrollTo({
-        top:0,
-        behavior:'smooth'
-    })
+    go_to_crud()
 }
 
 
@@ -346,10 +367,6 @@ function shadeColor(color, percent) {
       (Math.round((t - G) * p) + G) * 0x100 +
       (Math.round((t - B) * p) + B)).toString(16).slice(1)}`;
 }
-
-
-
-
 
 // go to crud scrolling down on click
 function go_to_crud(){
